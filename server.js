f6fc375cf76c2632f8d79e3094d63caaf27ba1fb
bharathsk1144-lang <bharathsk1144@gmail.com>
2026-08-30 17:59:15 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -4,22 +4,20 @@ const path = require("path");
 
 const app = express();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
 // Serve all files inside public folder
 app.use(express.static(path.join(__dirname, "public")));
 
-// Home page
+// Open index.html as homepage
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-// Server port
+// Render uses its own PORT
 const PORT = process.env.PORT || 5000;
 
-// Start server
 app.listen(PORT, () => {
   console.log(`ATMS Server running on port ${PORT}`);
 });
